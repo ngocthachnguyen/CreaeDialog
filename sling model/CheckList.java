6b6c970a4c9ed90c533dc180a10ce629925560bf
thach.nguyen <thach.nguyen@sutrixsolutions.com>
2018-02-07 17:47:29 +0700

    Add dialog

---
+++ sling model/CheckList.java
@@ -0,0 +1,41 @@
+package ph.com.globe.oc3.core.models;
+
+import javax.inject.Inject;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class CheckList {
+	/**
+	 * The icon image.
+	 */
+	@Inject
+	private String icon;
+	
+	/**
+	 * The description.
+	 */
+	@Inject
+	private String description;
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+}
